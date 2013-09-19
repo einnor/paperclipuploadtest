99c5846b6090e8c0efd900cee835b35f3cf36427
Ronnie Nyaga <rnafundi@gmail.com>
2013-09-19 17:57:31 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paperclipuploadtest::Application.config.secret_key_base = 'd445febe621178f98408977d7e38ad390828ce3947600c1e3eb30c43bd67ea1181b88938250a95a5ada951382ed21ae5b29a42a2f352d2bf07354244d2d5b1fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Paperclipuploadtest::Application.config.session_store :cookie_store, key: '_paperclipuploadtest_session'
